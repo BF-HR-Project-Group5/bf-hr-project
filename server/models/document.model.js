afa798a18954d5544983ab1c88efde44365d38b4
jmmon <jmmon93@gmail.com>
2023-01-28 11:27:16 -0800

    basic document service

--- server/models/document.model.js
+++ server/models/document.model.js
@@ -1,12 +1,13 @@
 
 const mongoose = require('mongoose');
+const { config } = require( '../config/constants' );
 const Schema = mongoose.Schema;
 
 const DocumentSchema = new Schema({
 		link: { type: String },
 		feedback: { type: String, maxlength: 256 },
-		status: { type: String, enum: documentStatus, default: 'PENDING' },
-		type: { type: String, enum: documentTypes, default: 'OPT' },
+		status: { type: String, enum: config.documentStatus, default: 'PENDING' },
+		type: { type: String, enum: config.documentTypes, default: 'F1(CPT/OPT)' },
 });
 
 const Document = mongoose.model('Document', DocumentSchema, 'Document');
--- server/services/documentService.js
+++ server/services/documentService.js
@@ -1,1 +1,15 @@
 // handles saving Document objects to Mongoose
+const Document = require('../models/document.model');
+
+const getDocumentById = async (id) => Document.findById(id);
+const getDocumentByLink = async (link) => Document.findOne({link});
+const getDocumentsByStatus = async (status) => Document.find({status});
+const getDocumentsByType = async (type) => Document.find({type});
+
+
+module.exports = {
+	getDocumentById,
+	getDocumentByLink,
+	getDocumentsByStatus,
+	getDocumentsByType,
+};
