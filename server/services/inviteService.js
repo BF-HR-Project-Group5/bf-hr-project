5548ce25657c887235a89372e9c087efb8595cc3
jmmon <jmmon93@gmail.com>
2023-01-28 12:48:33 -0800

    inviteService finished for now

--- server/services/inviteService.js
+++ server/services/inviteService.js
@@ -2,9 +2,13 @@ const { User } = require('../models');
 const Invite = require('../models/invite.model');
 const { caseInsensitiveRegex } = require('../utils/regexHelpers');
 
+// get singular invites
 const getInviteById = (id) => Invite.findById(id);
 const getInviteByEmail = (email) => Invite.findOne({ email });
+const getInviteByToken = (token) => Invite.findOne({ token });
+const getInviteByLink = (link) => Invite.findOne({ link });
 
+// get multiple invites
 const getInvitesByFirstName = (firstName) =>
 	Invite.find({ name: { first: caseInsensitiveRegex(firstName) } });
 const getInvitesByLastName = (lastName) =>
@@ -14,15 +18,29 @@ const getInvitesByMiddleName = (middleName) =>
 const getInvitesByPreferredName = (preferredName) =>
 	Invite.find({ name: { preferred: caseInsensitiveRegex(preferredName) } });
 
-const getInviteByToken = (token) => Invite.findOne({ token });
-const getInviteByLink = (link) => Invite.findOne({ link });
+const queryInvites = (filter) => Invite.find(filter);
 
 // get invites that are registered; or can pass in `false` to get unregistered invites
-const getInvitesByRegistered = (isRegistered = true) => Invite.find({ isRegistered: isRegistered });
+const getInvitesByIsRegistered = (isRegistered = true) =>
+	Invite.find({ isRegistered: isRegistered });
+
+const getInvitesByFullName = async (fullName) => {
+	const names = fullName.split(' ');
+
+	const filter = {
+		$or: [
+			{ name: { first: { $or: names } } },
+			{ name: { last: { $or: names } } },
+			{ name: { middle: { $or: names } } },
+			{ name: { preferred: { $or: names } } },
+		],
+	};
+
+	return queryInvites(filter);
+};
 
 // when an HR person creates an invite, the email should be unique.
 // (Afterwards, when the user registers using the link, they need to use (this email or ) a unique email, and also a unique username)
-// data: {email, token, name: {first, last, middle, preferred}, link, isRegistered: false,}
 const createInvite = async (
 	data = {
 		email: '',
@@ -39,4 +57,28 @@ const createInvite = async (
 
 	// create it?
 	return Invite.create(data);
+};
+
+const putIsRegisteredToInviteId = async (id) => {
+	const invite = getInviteById(id);
+	invite.isRegistered = true;
+	await invite.save();
+	return invite;
+};
+
+module.exports = {
+	getInviteById,
+	getInviteByEmail,
+	getInviteByToken,
+	getInviteByLink,
+
+	getInvitesByFirstName,
+	getInvitesByLastName,
+	getInvitesByMiddleName,
+	getInvitesByPreferredName,
+	queryInvites,
+	getInvitesByIsRegistered,
+	getInvitesByFullName,
+	createInvite,
+	putIsRegisteredToInviteId,
 };
