ebc1903ea952313b37ec841ee1949d3438e5c5cc
jmmon <jmmon93@gmail.com>
2023-01-31 11:21:27 -0800

    seed profiles/documents; seeding is working!

--- server/config/seed.js
+++ server/config/seed.js
@@ -7,13 +7,15 @@ const { MONGO_URL } = process.env;
 const inviteService = require('../services/inviteService');
 const Invite = require('../models/invite.model');
 const userService = require('../services/userService');
-const User = require( '../models/user.model' );
+const User = require('../models/user.model');
 const houseService = require('../services/houseService');
-const House = require( '../models/house.model' );
+const House = require('../models/house.model');
+const profileService = require('../services/profileService');
+const Profile = require('../models/profile.model');
+const documentService = require('../services/documentService');
+const Document = require('../models/document.model');
 
 const seedInvites = async (count) => {
-	// create a handful of invites
-	// save them...
 	const invites = [];
 	for (let i = 0; i < count; i++) {
 		const data = {
@@ -27,59 +29,39 @@ const seedInvites = async (count) => {
 			isRegistered: false,
 		};
 		invites.push(inviteService.createInvite(data));
-		// console.log('pushing invite:', data);
 	}
 	const results = await Promise.all(invites);
-	// console.log('inviteResults:', { results });
 	return results;
 };
 
-const seedInvitedUsers = async (count, invites, role = 'user') => {
-	// create a user for each invite
-
+const seedInvitedUsers = async (count, invites, houses, profiles, role = 'user') => {
 	const users = [];
 	for (let i = 0; i < count; i++) {
 		const data = {
+			email: `email${i}@email.com`,
+			username: `username${i}`,
+			password: `password${i}`, // not pass validation once implemented
+			// password: `P4$$w0rd${i}`, // passes validation
+
 			name: {
 				first: `first${i}`,
 				middle: `middle${i}`,
 				last: `last${i}`,
 				preferred: `preferred${i}`,
 			},
-			email: `email${i}@email.com`,
-			username: `username${i}`,
-			password: `password${i}`, // not pass validation once implemented
-			// password: `P4$$w0rd${i}`, // passes validation
-			ssn: Number(`99999999${i}`),
-			address: {
-				line1: `addressLine1_${i}`,
-				line2: `addressLine2_${i}`,
-				city: `city_${i}`,
-				state: `state_${i}`,
-				zipcode: `55555(-5555)${i}`,
-			},
-			/// workAuth
-			/// license
-			phone: {
-				mobile: Number(`999999999${i}`),
-				// work
-			},
+
 			role,
 			invite: invites[i]._id, // add the created invite
+			profile: profiles[i]._id,
+			house: houses[i]._id,
 		};
 		users.push(userService.createUser(data));
-		// console.log('pushing user:', data);
 	}
 	const results = await Promise.all(users);
-	// console.log('userResults:', { results });
-	// now should have users and invites!
 	return results;
 };
 
-
 const seedHouses = async (count) => {
-	// create a user for each invite
-
 	const houses = [];
 	for (let i = 0; i < count; i++) {
 		const data = {
@@ -105,40 +87,124 @@ const seedHouses = async (count) => {
 			facilityReports: [],
 		};
 		houses.push(houseService.createHouse(data));
-		// console.log('pushing house:', data);
 	}
 	const results = await Promise.all(houses);
-	// console.log('houseResults:', { results });
-	// now should have users and invites!
 	return results;
 };
 
+const seedProfiles = async (count, documents) => {
+	const profiles = [];
+	for (let i = 0; i < count; i++) {
+		const data = {
+			ssn: Number(`99999999${i}`),
+			address: {
+				line1: `addressLine1_${i}`,
+				line2: `addressLine2_${i}`,
+				city: `city_${i}`,
+				state: `state_${i}`,
+				zipcode: `55555(-5555)${i}`,
+			},
+			workAuth: {
+				title: '',
+				startDate: '',
+				endDate: '',
+				daysRemaining: 99,
+			},
+			car: {
+				make: 'Tesla',
+				model: 'Roadster',
+				color: 'Silver',
+			},
+			license: {
+				number: `${i}abcde12345`,
+				expiration: new Date(),
+				document: documents[i+count],
+			},
+			phone: {
+				mobile: Number(`999999999${i}`),
+				// work
+			},
+			documents: [documents[i]],
+			feedback: 'overall feedback on profile',
+			emergencyContact: [
+				{
+					name: {
+						first: `first${i}`,
+						last: `last${i}`,
+						middle: `middle${i}`,
+					},
+					phone: `999999999${i}`,
+					email: `email${i}@email.com`,
+					relationship: `BFF`,
+				},
+			],
+			status: `PENDING`,
+		};
+		profiles.push(profileService.createProfile(data));
+	}
+	const results = await Promise.all(profiles);
+	return results;
+};
+
+const seedDocuments = async (count) => {
+	const documents = [];
+	for (let i = 0; i < count; i++) {
+		const data = {
+			link: `www.some.aws.link.com`,
+			feedback: 'feedback on document',
+			status: `PENDING`,
+			type: `F1(CPT/OPT)`,
+		};
+		documents.push(documentService.createDocument(data));
+	}
+	return Promise.all(documents);
+};
+
+const updateHouseRoommates = async (count, users, houses) => {
+	const houseUpdates = [];
+	for (let i = 0; i < count; i++){
+		houseUpdates.push(houseService.addUserIdToHouseId(users[i]._id, houses[i]._id));
+	}
+	return Promise.all(houseUpdates);
+}
 
 const clearInvites = () => Invite.deleteMany({});
-const clearUsers = () => User.deleteMany({_id: {$ne: '63d6da694ba462d927745b7e'}});
+const clearUsers = () => User.deleteMany({ _id: { $ne: '63d6da694ba462d927745b7e' } });
 const clearHouses = () => House.deleteMany({});
+const clearProfiles = () => Profile.deleteMany({});
+const clearDocuments = () => Document.deleteMany({});
 
 async function run() {
 	try {
 		// connect mongoose
+		const count = 5; // how many of each to make
+		const documentCount = count * 2 // one license doc, one work auth doc
 		mongoose.set('strictQuery', false);
 		await mongoose.connect(MONGO_URL);
 		console.log('Seed - Connected to DB');
 
-		await clearInvites();
-		await clearUsers();
-		await clearHouses();
+		await Promise.allSettled([clearInvites(), clearUsers(), clearHouses(), clearProfiles(), clearDocuments()])
 
-		const houses = await seedHouses(5);
+		// seed documents, houses, invites
+		// const documents = await seedDocuments(5);
+		// const houses = await seedHouses(5);
+		// const invites = await seedInvites(5);
+		const [documents, houses, invites] = (await Promise.allSettled([seedDocuments(documentCount), seedHouses(count), seedInvites(count)])).map(result => result.value);
 
-		const invites = await seedInvites(5);
+		console.log({documents, houses, invites});
 
-		const users = await seedInvitedUsers(5, invites);
+		// seed profiles and add a document
+		const profiles = await seedProfiles(count, documents);
+
+		// seed users and add invites, houses, profiles
+		const users = await seedInvitedUsers(count, invites, houses, profiles);
+
+		// update house roommates
+		await updateHouseRoommates(count, users, houses);
+
 		console.log('final users:', users);
-
 	} catch (e) {
-		console.log('error seeding invites and users:', e);
-
+		console.log('error running seed:', e);
 	} finally {
 		console.log('~~ seed completed');
 		mongoose.connection.close();
