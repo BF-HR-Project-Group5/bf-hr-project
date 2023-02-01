ee3b247969d6178f086d0ba4c4ff752b2ea8ffdb
y3589358 <tyang28@hawk.iit.edu>
2023-01-31 19:27:43 -0600

    Built Visa Status Management page

---
+++ client/src/pages/Visa-Status.js
@@ -0,0 +1,132 @@
+import React, { useState, useEffect } from "react";
+import '../layout/Visa-Status.css'
+import { connect } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Navigation from '../components/navigation/navigation';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+    },
+    button: {
+      marginRight: theme.spacing(1),
+    },
+    instructions: {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+    },
+  }));
+  
+  function getSteps() {
+    return ['OPT Receipt', 'OPT EAD', 'I-983', 'I-20'];
+  }
+  
+  function getStepContent(step) {
+    switch (step) {
+      case 0:
+        return 'Please upload a copy of your OPT EAD...';
+      case 1:
+        return 'Please download and fill out the I-983 form';
+      case 2:
+        return 'Please send the I-983 along with all necessary documents to your school and upload the new I-20!';
+      default:
+        return 'All documents have been approved';
+    }
+  }
+
+const VisaStatus = (props) => {
+    console.log('props',props)
+
+    const classes = useStyles();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const [skipped, setSkipped] = React.useState(new Set());
+    const steps = getSteps();
+  
+    const isStepOptional = (step) => {
+      return step === 1;
+    };
+  
+    const handleNext = () => {
+      let newSkipped = skipped;
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+      setSkipped(newSkipped);
+    };
+  
+    const handleBack = () => {
+      setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+  
+    const handleSkip = () => {
+      if (!isStepOptional(activeStep)) {
+        // You probably want to guard against something like this,
+        // it should never occur unless someone's actively trying to break something.
+        throw new Error("You can't skip a step that isn't optional.");
+      }
+  
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+      setSkipped((prevSkipped) => {
+        const newSkipped = new Set(prevSkipped.values());
+        newSkipped.add(activeStep);
+        return newSkipped;
+      });
+    };
+  
+    const handleReset = () => {
+      setActiveStep(0);
+    };
+  
+    return (
+      <div className={classes.root}>
+        <Navigation />
+        <Stepper activeStep={activeStep}>
+          {steps.map((label, index) => {
+            const stepProps = {};
+            const labelProps = {};
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+        <div className="container">
+          {activeStep === steps.length ? (
+            <div>
+              <Typography className={classes.instructions}>
+                All steps completed - you&apos;re finished
+              </Typography>
+            </div>
+          ) : (
+            <div>
+              <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+              <div>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleNext}
+                  className={classes.button}
+                >
+                  {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+}
+
+// export default PersonalInformation;
+const mapStateToProps = ({ auth }) => ({
+    auth
+});
+
+export default connect(
+    mapStateToProps
+)(VisaStatus);
+
