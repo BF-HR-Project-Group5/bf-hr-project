ee621e207c03813df83f63cdc4284d04d765e37f
Joe Moulton <67028427+jmmon@users.noreply.github.com>
2023-02-02 12:33:32 -0800

    Delete index.js

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-exports.authRouter = require('./authRouter');
-exports.houseRouter = require('./houseRouter');
-exports.inviteRouter = require('./inviteRouter');
-// exports.userRouter = require('./userRouter');
-exports.facilityRouter = require('./facilityRouter');
-// exports.testRouter = require('./testRouter.js')
-exports.s3Router = require('./s3Router.js');
-exports.profileRouter = require('./profileRouter');
