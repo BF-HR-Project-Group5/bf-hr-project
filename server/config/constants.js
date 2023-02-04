547613a2e3ac26f64777894b3df1e5ab5333364f
jmmon <jmmon93@gmail.com>
2023-02-03 20:17:26 -0800

    built other constant object

--- server/config/constants.js
+++ server/config/constants.js
@@ -27,12 +27,62 @@ exports.config = {
 
 		genders: ['MALE' , 'FEMALE' , 'NO_RESPONSE'],
 
+
+		nextStepCode: [
+			'OPT_RECEIPT_WAIT', // uploaded, next step is wait for approval
+			'OPT_EAD_UPLOAD', // next step is upload next doc
+			'OPT_EAD_WAIT',
+			'I-983_UPLOAD',
+			'I-983_WAIT',
+			'I-20_UPLOAD',
+			'I-20_WAIT',
+			'COMPLETED'
+		],
+		
+		nextSteps: {
+			// should have 1 document minimum, so the first phase is waiting for HR approval
+			OPT_RECEIPT_WAIT: {
+				user: 'Please wait for HR to approve your OPT receipt.',
+				hr: 'OPT receipt needs approval.'
+			},
+			OPT_EAD_UPLOAD: {
+				user: 'Please upload your OPT EAD.',
+				hr: 'Waiting on OPT EAD upload.'
+			},
+			OPT_EAD_WAIT: {
+				user: 'Please wait for HR to approve your OPT EAD.',
+				hr: 'OPT EAD needs approval.'
+			},
+			'I-983_UPLOAD': {
+				user: 'Please upload your I-983.',
+				hr: 'Waiting on I-983 upload.'
+			},
+			'I-983_WAIT': {
+				user: 'Please wait for HR to approve your I-983.',
+				hr: 'I-983 needs approval.'
+			},
+			'I-20_UPLOAD': {
+				user: 'Please upload your I-20.',
+				hr: 'Waiting on I-20 upload.'
+			},
+			'I-20_WAIT': {
+				user: 'Please wait for HR to approve your I-20.',
+				hr: 'I-20 needs approval.'
+			},
+			'COMPLETE': {
+				user: 'You are all done!',
+				hr: 'Document upload complete.'
+			},
+		
+		}
+
 		// for "Next Step"
 		steps: [
 			'OPT_RECEIPT_WAITING', // uploaded, next step is wait for approval
 			'OPT_RECEIPT_APPROVED', // next step is upload next doc
 			'OPT_EAD_WAITING',
 			'OPT_EAD_APPROVED',
+<<<<<<< Updated upstream
 			'I-983_WAITING',
 			'I-983_APPROVED',
 			'I-20_WAITING',
@@ -48,6 +98,12 @@ exports.config = {
 			'I-983_WAIT',
 			'I-20_UPLOAD',
 			'I-20_WAIT',
+=======
+			'I-984_WAITING',
+			'I-984_APPROVED',
+			'I-21_WAITING',
+			'I-21_APPROVED', // i-20 is approved? all done
+>>>>>>> Stashed changes
 			'COMPLETED'
 		],
 
