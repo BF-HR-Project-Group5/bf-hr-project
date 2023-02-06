c6476c5658c28aea8575b2e03b4a8d437f2a9015
jmmon <jmmon93@gmail.com>
2023-02-05 17:50:34 -0800

    at least there's something on the home page!

--- client/src/pages/HrHome.js
+++ client/src/pages/HrHome.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import {Link} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 
 function HrHome(props) {
 	console.log('hrHome props.auth', props.auth);
 	return (
-		<div>
-			<h1>HR Home page</h1>
+		<div className="container-fluid p-5">
+			<div className="row mt-3">
+				<div className="col-12 mx-auto text-center">
+					<h1 className="text-center">HR Home page</h1>
+				</div>
+			</div>
+			<div className="row mt-3">
+				<ul className="col-4 mx-auto text-center list-group">
+					<li className="list-group-item"><Link to="/employeeProfiles" >Employee Profiles</Link></li>
+					<li className="list-group-item"><Link to="/hrVisaStatus" >Visa Status Management</Link></li>
+					<li className="list-group-item"><Link to="/hiringManagement" >Hiring Management</Link></li>
+					<li className="list-group-item"><Link to="/housingManagement" >Housing Management</Link></li>
+				</ul>
+			</div>
 		</div>
 	)
 }
