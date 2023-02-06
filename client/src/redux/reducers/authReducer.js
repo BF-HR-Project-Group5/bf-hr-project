9431fb94cf0313a9a458077f01e7df3c790920fb
jmmon <jmmon93@gmail.com>
2023-02-06 14:44:40 -0800

    not working...

--- client/src/redux/reducers/authReducer.js
+++ client/src/redux/reducers/authReducer.js
@@ -3,7 +3,7 @@ import { FETCH_USER } from '../actions/types';
 export default function(state = null, action) {
   switch (action.type) {
     case FETCH_USER:
-      return action.payload;
+      return {...state, auth: action.payload};
     default:
       return state;
   }
